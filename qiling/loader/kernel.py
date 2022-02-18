111c0c87c9ebfd6526dbdafb32bea39a6bb40088
dataisland <dataisland@outlook.com>
2022-02-18 23:15:33 +0800

    Add kernel loader

--- qiling/loader/kernel.py
+++ qiling/loader/kernel.py
@@ -3,6 +3,8 @@
 # Cross Platform and Multi Architecture Advanced Binary Emulation Framework
 # Built on top of Unicorn emulator (www.unicorn-engine.org) 
 
+import lief
+
 from .loader import QlLoader
 
 
@@ -10,5 +12,16 @@ class QlLoaderKERNEL(QlLoader):
     def __init__(self, ql):
         super().__init__(ql)
 
+        self.elf = lief.parse(ql.path)
+
+    @property
+    def entry_point(self):
+        return self.elf.entrypoint
+
     def run(self):
-        pass
+        for segment in self.elf.segments:
+            if segment.type == lief.ELF.SEGMENT_TYPES.LOAD:
+                self.ql.mem.map(segment.virtual_address, segment.virtual_size)            
+            
+                for section in segment.sections:
+                    self.ql.mem.write(section.virtual_address, bytes(section.content))
