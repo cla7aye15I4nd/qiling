7d5c9d6754c31016622a30775ea02479cc4684c4
dataisland <dataisland@outlook.com>
2022-02-24 13:48:31 +0800

    Fix load_all bug

--- qiling/hw/hw.py
+++ qiling/hw/hw.py
@@ -61,7 +61,7 @@ class QlHwManager:
     def load_all(self):
         for label, args in self.ql.env.items():
             if args['type'] == 'peripheral':
-                self.create(label, args['struct'], args['base'], args.get("kwargs", {}))
+                self.create(label.lower(), args['struct'], args['base'], args.get("kwargs", {}))
 
     def find(self, address: int):
         """ Find the peripheral at `address`
